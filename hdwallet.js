9efa10dfa213fd9d820ca831126871168c4a4d1c
Stanly Johnson <stanlyjohnson@outlook.com>
2018-12-12 16:35:57 +0700

    mods

--- hdwallet.js
+++ hdwallet.js
@@ -7,45 +7,42 @@ const web3 = new Web3(
    new Web3.providers.HttpProvider("https://rinkeby.infura.io/yGEHQFbey55ozzDha3hf")
 );
 
-const mnemonic = bip39.generateMnemonic(); //generates string
+//const mnemonic = bip39.generateMnemonic(); //generates string
+const mnemonic = "hold warrior physical fun excuse resource capable scissors pyramid hip jungle minor";
+
 console.log("Mnemonic:",mnemonic);
 
 const seed = bip39.mnemonicToSeed(mnemonic); //creates seed buffer
+//console.log(seed.toString('hex'));
 
 const root = hdkey.fromMasterSeed(seed)
 
 const masterPrivateKey = root.privateKey.toString('hex');
-console.log("Master private Key:",masterPrivateKey);
+//console.log("Master private Key:",masterPrivateKey);
 
 const masterPubKey = root.publicKey.toString('hex');
-console.log("Master Public Key: ",masterPubKey);
+//console.log("Master Public Key: ",masterPubKey);
 
 var path = "m/44'/60'/0'/0/0";
 
 const addrNode = root.derive(path)
-console.log("path: ", path);
-
-const pubKey = ethUtil.privateToPublic(addrNode._privateKey)
-console.log("Pubkey as hex:",pubKey.toString('hex'));
-
+console.log("privatekey",addrNode._privateKey.toString('hex'));
+const pubKey = ethUtil.privateToPublic(addrNode._privateKey);
 const addr = ethUtil.publicToAddress(pubKey).toString('hex');
-console.log("pubkey to Addr:",addr);
+console.log("Address:",addr);
 
-const PrivBuffKey = Buffer.from(masterPrivateKey, 'hex')
 const rawTransaction = {
   nonce:0x6,
   to: '0x4584159875418ef77D1142bEec0b6648BD8eDb2f',
   value: '0x00',
-  gasPrice: 0x09184e72a000,
-  gasLimit: 0x30000,
+  gasPrice: "0x09184e72a000",
+  gasLimit: "0x9C40",
   chainId: 4
 }
 
 var transaction = new ethTx(rawTransaction);
 transaction.sign(addrNode._privateKey);
 const serializedTx = transaction.serialize()
-
-console.log('Serialized transaction:',serializedTx.toString('hex'));
 
 web3.eth.sendRawTransaction(
    `0x${serializedTx.toString('hex')}`,
